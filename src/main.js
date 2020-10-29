0a0262524ce280e632d4f30b978f3f48e2b6b3e1
lucasfullstackdev <lucas.fullstack.dev@gmail.com>
2020-10-29 13:38:55 -0300

    instalacao do vue-router

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
